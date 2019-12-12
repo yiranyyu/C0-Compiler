ed985b3626a27e194462240592929e490db77423
yiranyyu <2606375857@qq.com>
2019-12-12 23:57:04 +0800

    Add type-check function

--- src/elf/pcode.py
+++ src/elf/pcode.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 class PCode(object):
     SNEW = 'SNEW'
 
@@ -7,7 +10,7 @@ class PCode(object):
     CSCAN = 'CSCAN'
     IPRINT = 'IPRINT'
     DPRINT = 'DPRINT'
-    CPRINT = 'CPRING'
+    CPRINT = 'CPRINT'
     SPRINT = 'SPRINT'
     PRINTL = 'PRINTL'
 
@@ -27,6 +30,7 @@ class PCode(object):
     # load
     LOADA = 'LOADA'
     ILOAD = 'ILOAD'
+    DLOAD = 'DLOAD'
     LOADC = 'LOADC'
 
     # store
@@ -58,14 +62,256 @@ class PCode(object):
     ICMP = 'ICMP'
     DCMP = 'DCMP'
 
-    type_to_size = {
+    type_to_info: Dict[str, dict] = {
+        SNEW: {
+            'size': 1 + 4,
+            'operands': 1,
+            'code': 0x0c
+        },
 
+        ISCAN: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xb0
+        },
+        DSCAN: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xb1
+        },
+        CSCAN: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xb2
+        },
+        IPRINT: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xa0
+        },
+        DPRINT: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xa1
+        },
+        CPRINT: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xa2
+        },
+        SPRINT: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xa3
+        },
+        PRINTL: {
+            'size': 1,
+            'operands': 0,
+            'code': 0xaf
+        },
+
+        CALL: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x80
+        },
+        RET: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x88
+        },
+        IRET: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x89
+        },
+        DRET: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x8a
+        },
+        JE: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x71
+        },
+        JNE: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x72
+        },
+        JL: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x73
+        },
+        JLE: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x76
+        },
+        JG: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x75
+        },
+        JGE: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x74
+        },
+        JMP: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x70
+        },
+
+        LOADA: {
+            'size': 1 + 2 + 4,
+            'operands': 2,
+            'code': 0x0a
+        },
+        ILOAD: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x10
+        },
+        DLOAD: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x11
+        },
+        LOADC: {
+            'size': 1 + 2,
+            'operands': 1,
+            'code': 0x09
+        },
+        ISTORE: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x20
+        },
+        DSTORE: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x21
+        },
+
+        I2D: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x60
+        },
+        D2I: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x61
+        },
+        I2C: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x62
+        },
+
+        DADD: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x31
+        },
+        IADD: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x30
+        },
+        DSUB: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x35
+        },
+        ISUB: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x34
+        },
+        DMUL: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x39
+        },
+        IMUL: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x38
+        },
+        DDIV: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x3d
+        },
+        IDIV: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x3c
+        },
+        DNEG: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x41
+        },
+        INEG: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x40
+        },
+
+        BIPUSH: {
+            'size': 1 + 1,
+            'operands': 1,
+            'code': 0x01
+        },
+        IPUSH: {
+            'size': 1 + 4,
+            'operands': 1,
+            'code': 0x02
+        },
+
+        ICMP: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x44
+        },
+        DCMP: {
+            'size': 1,
+            'operands': 0,
+            'code': 0x45
+        },
     }
 
     def __init__(self, inst_type: str, *ops):
+        # print(f'create {inst_type} with {ops}')
         self.operator = inst_type
         self.operands = ops
-        self.size = PCode.type_to_size[inst_type]
+        self.size = PCode.type_to_info[inst_type]['size']
+        self.__check_syntax()
 
     def update(self, *args):
+        # print(f'Update from `{self}` to ', end='')
         self.operands = args
+        # print(f'`{self}`\n')
+        self.__check_syntax()
+
+    def __check_syntax(self):
+        info = PCode.type_to_info[self.operator]
+        operands = info['operands']
+
+        error_msg = f'{operands} operands need of {self.operator}, but got {len(self.operands)}'
+        assert (len(self.operands) == operands), error_msg
+
+    def __str__(self):
+        output = ''
+        output += self.operator
+        for idx, x in enumerate(self.operands):
+            if idx:
+                output += ','
+            output += f' {x}'
+        return output
